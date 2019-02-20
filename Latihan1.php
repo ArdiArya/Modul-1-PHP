b21ca13b978ba682590d6fb78396c021e70df39e
ardi_arya_27rpl <ardi_arya_27rpl@student.smktelkom-mlg.sch.id>
2019-02-20 14:21:51 +0700

    sampun

---
+++ Latihan1.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Namaku Ardi Arya";
+    echo "<br>";
+    echo "Aku Sedang Belajar PHP Di SMK Telkom Malang";
+?>
---
+++ Latihan3.php
@@ -0,0 +1,6 @@
+<?php
+    $sekolah = "SMK Telkom Malang";
+    $angkatan = 27;
+    echo "Saya Adalah Siswa " .$sekolah;
+    echo " angkatan" .$angkatan;
+?>
